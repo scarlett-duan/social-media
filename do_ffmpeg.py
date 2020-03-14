42c0871c741c88d1dffa541459352c6292d34bfb
VibratoWWW <50663675+VibratoWWW@users.noreply.github.com>
2020-03-14 20:56:37 +0800

    Update do_ffmpeg.py

--- do_ffmpeg.py
+++ do_ffmpeg.py
@@ -1,13 +1,13 @@
 import os
 
-ini_folder = './weibo_data/data_1/'
-for i in range(1, 6):
+ini_folder = './tencent_data/'
+for i in range(0, 6):
     try:
-        foldername = ini_folder + str(i) + '_upload/1_video/'
-        filename = foldername + str(i)  # + '.mp4'
-        save_name = ini_folder + str(i) + '_upload/2_audio/'
-        cmd = 'ffmpeg -vn -y -acodec copy ' + \
-            save_name + ' -i ' + filename + str(i) + '.m4a'
+        foldername = ini_folder + str(i) + '_upload/1_Video/'
+        filename = foldername + str(i) + '.mp4'
+        save_name = ini_folder + str(i) + '_upload/2_Audio/'
+        cmd = 'ffmpeg -i ' + filename + \
+            ' -vn -acodec copy ' + save_name + str(i) + '.m4a'
         os.system(cmd)
     except Exception as err:
         print(err)
